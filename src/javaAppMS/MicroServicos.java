08d364a190c5fed60706aca6c6d0381541907bdd
Guilherme Portella <guilhermeportella2@gmail.com>
2022-08-09 17:18:37 -0300

    Update MicroServicos.java

--- src/javaAppMS/MicroServicos.java
+++ src/javaAppMS/MicroServicos.java
@@ -15,7 +15,6 @@ public class MicroServicos {
     
     
     public static void main(String[] args) {
-        // TODO code application logic here
         System.out.println("concept test:");
         System.out.println("code engineering");
     }
