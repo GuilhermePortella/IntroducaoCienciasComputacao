afa261cad6102d63e7a22afb828bd76167f785a9
Guilherme Portella <guilhermeportella2@gmail.com>
2022-08-14 15:50:00 -0300

    Update Exercicio4EsSequencial.java

--- src/basic_Java/Exercicio4EsSequencial.java
+++ src/basic_Java/Exercicio4EsSequencial.java
@@ -1,17 +1,8 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package basic_Java;
 
 import java.util.Locale;
 import java.util.Scanner;
 
-/**
- *
- * @author Guilherme
- */
 public class Exercicio4EsSequencial {
 
     public static void main(String[] args) {
