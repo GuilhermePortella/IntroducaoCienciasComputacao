c9f806dec96385deee70cd2c26e643538e61833f
Guilherme Portella <guilhermeportella2@gmail.com>
2022-09-17 22:16:10 -0300

    Update IntroducaoCienciasComputacao.java

--- src/introducaocienciascomputacao/IntroducaoCienciasComputacao.java
+++ src/introducaocienciascomputacao/IntroducaoCienciasComputacao.java
@@ -10,7 +10,6 @@ public class IntroducaoCienciasComputacao {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
         System.out.println("Ola mundo");
     }
     
